98c5eab0aa3e50bdf3f8cd9d92a12255ee2a13e5
pupunzi <pupunzi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-19 16:27:13 +0000

    added v.0.1.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-miniaudioplayer/tags/0.1@614598 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- miniAudioPlayer.php
+++ miniAudioPlayer.php
@@ -4,11 +4,11 @@ Plugin Name: mb.miniAudioPlayer
 Plugin URI: http://pupunzi.com/#mb.components/mb.miniAudioPlayer/miniAudioPlayer.html
 Description: Transform your mp3 audio file link into a nice, small light player
 Author: Pupunzi (Matteo Bicocchi)
-Version: 0.1.1
+Version: 0.1
 Author URI: http://pupunzi.com
 */
 
-define("miniAudioPlayer_VERSION", "0.1.1");
+define("miniAudioPlayer_VERSION", "0.1");
 
 register_activation_hook( __FILE__, 'miniAudioPlayer_install' );
 
