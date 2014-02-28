8347a324a8ebeb206081b5048a1e517cb6b48bf5
pupunzi <pupunzi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-28 16:17:40 +0000

    changed HTML declaration
    git-svn-id: https://plugins.svn.wordpress.org/wp-miniaudioplayer/trunk@867033 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-miniaudioplayer/maptinymce/maplayertinymce.php
+++ wp-miniaudioplayer/maptinymce/maplayertinymce.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE HTML>
 <?php
 require('../../../../wp-blog-header.php');
 $plugin_version = get_option('mbYTPlayer_version');
