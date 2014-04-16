43452cae8af631fb923d0806f7666d2f7db9e303
pupunzi <pupunzi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-16 21:18:51 +0000

    = 1.5.3 = * Major update: Updated to solve issue compatibilities with WP 3.9 - Needed if you are updating your Wordpress to the latest 3.9 release.
    git-svn-id: https://plugins.svn.wordpress.org/wp-miniaudioplayer/tags/1.5.3@895131 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ maptinymce/tinymcemaplayer.js
@@ -0,0 +1,54 @@
+(function() {
+	tinymce.create('tinymce.plugins.maplayer', {
+
+		init : function(ed, url) {
+
+			var popUpURL = url + '/maplayertinymce.php';
+
+			ed.addCommand('maplayerpopup', function() {
+				if(ed.canOpenPopUp)
+					ed.windowManager.open({
+						url : popUpURL,
+						width : 600,
+						height : 820,
+						inline : 1
+					});
+				else
+					alert("You must select a link to an .mp3 file first.")
+			});
+
+			ed.addButton('maplayerbutton', {
+				title : 'Modify a miniAudioPlayer',
+				image : url + '/maplayerbutton.png',
+				cmd : 'maplayerpopup'
+			});
+
+			ed.onNodeChange.add(function(ed) {
+				var selection = ed.selection.getNode();
+
+				var btnId = typeof ed.controlManager.buttons != "undefined" ? ed.controlManager.buttons.maplayerbutton._id : ed.controlManager.get("maplayerbutton").id;
+
+				var disable = false;
+				ed.canOpenPopUp = false;
+
+				jQuery("#"+btnId).css({opacity:.5, border:"1px solid transparent"});
+				if (jQuery(selection).is("a[href *= '.mp3']") || jQuery(selection).find("a[href *= '.mp3']").lenght>0 || jQuery(selection).prev().is("a[href *= '.mp3']")) {
+					ed.canOpenPopUp = true;
+					disable = false;
+					jQuery("#"+btnId).css({border:"1px solid gray", opacity:1});
+				}
+				ed.controlManager.setDisabled("maplayerbutton", disable);
+
+			});
+		},
+
+		createControl : function() {
+			return null;
+		},
+
+		getInfo : function() {
+			return {};
+		}
+	});
+	tinymce.PluginManager.add('maplayer', tinymce.plugins.maplayer);
+}());
